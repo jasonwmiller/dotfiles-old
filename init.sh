edd2707ff4e2967dc57c1d35fece92213d301b88
Jason W. Miller <jwm@jasonwmiller.info>
2012-07-30 16:03:20 +0000

    final setup scrip

--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 echo "setting up links to dofiles";
 ln -s ~/dotfiles/zshrc ~/.zshrc;
-ln -s ~/dotfiles/vim ~/.vim;
-echo " initing vim plugins";
+ln -s ~/dotfiles/.vim ~/.vim;
+ln -s ~/dotfiles/vimrc.local ~/.vimrc.local;
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf;
+echo " initing vim and plugins";
 cd ~/dotfiles;
 git submodule update --init;
-
+cd ~/dotfiles/.vim
+git submodule add git://github.com/jasonwmiller/tmux-config.git bundle/tmux-config
+git submodule update --init
+ln -s ~/.vim/bundle/tmux-config/tmux.conf ~/.tmux.conf
 echo "don't forget to install oh-my-zsh";
 echo "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh";
