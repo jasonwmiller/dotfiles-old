8022a38b3e1e754ffd0291bd9cf9a16d3f6c7741
Jason W. Miller <jwm@jasonwmiller.info>
2012-07-30 07:06:09 +0000

    build

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo "installing oh-my-zsh";
-echo "setting up dofiles";
-ln -s ~/dotfiles/zshrc ~/zshrci;
+echo "setting up links to dofiles";
+ln -s ~/dotfiles/zshrc ~/.zshrc;
 ln -s ~/dotfiles/vim ~/.vimrc;
 echo " initing vim plugins";
 cd ~/dotfiles;
 git submodule update --init;
+
+echo "don't forget to install oh-my-zsh";
+echo "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh";
